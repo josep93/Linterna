9a53e9244c205754f5f0ce4f55121e4bf2639a8b
Jesús Miguel Pinto Sáez <jesuspinto@signlab.info>
2021-01-29 21:58:33 +0100

    aiming

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject player;
+    public Camera camera;
+    private PlayerInputs controls;
+    // Update is called once per frame
+
+    private void Start()
+    {
+        controls = new PlayerInputs();
+        controls.Player.Enable();
+    }
+
+    void Update()
+    {
+        AimTorch();
+    }
+
+    private void AimTorch()
+    {
+        var aimVec = controls.Player.MousePosition.ReadValue<Vector2>();
+
+        var aimVecWorld = camera.ScreenToWorldPoint(new Vector3(aimVec.x, aimVec.y, 10.0f));
+        Debug.Log(aimVecWorld);
+        aimVecWorld = (Vector3)aimVecWorld - player.transform.position;
+        var aimAngle = Mathf.Atan2(aimVecWorld.x, aimVecWorld.z) * 180 / Mathf.PI;
+        player.transform.eulerAngles = new Vector3(player.transform.rotation.eulerAngles.x,aimAngle, player.transform.rotation.eulerAngles.z);
+    }
+}
