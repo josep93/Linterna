37c719274abc93a86d0cad77e7cbe029200c6e20
josep93 <joseluisgomezmino@hotmail.com>
2021-01-29 20:00:39 +0100

    Menu - Creación de menu

---
+++ Assets/Scripts/Menu/MenuScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    
+    public void jugar()
+    {
+        SceneManager.LoadScene("New Scene");
+    }
+
+    public void salir()
+    {
+        Application.Quit();
+    }
+
+}
