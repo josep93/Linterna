d1b588df8e8c0f25db260f170896bd7f4b44aa8f
josep93 <joseluisgomezmino@hotmail.com>
2021-01-30 00:50:32 +0100

    Menu - Implementado luces

--- Assets/Scripts/Menu/MenuScript.cs
+++ Assets/Scripts/Menu/MenuScript.cs
@@ -3,7 +3,8 @@ using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
-    
+    public GameObject miLuz;
+
     public void jugar()
     {
         SceneManager.LoadScene("New Scene");
@@ -12,6 +13,27 @@ public class MenuScript : MonoBehaviour
     public void salir()
     {
         Application.Quit();
+    }
+
+    public void changeButtong(int button)
+    {
+
+        switch (button)
+        {
+            case 0:
+                //-6 5.75 -13
+                miLuz.transform.localPosition = new Vector3(miLuz.transform.localPosition.x, 5.75f, miLuz.transform.localPosition.z);
+                break;
+
+            case 1:
+                //-6 2.75 -13
+                miLuz.transform.localPosition = new Vector3(miLuz.transform.localPosition.x, 2.75f, miLuz.transform.localPosition.z);
+                break;
+
+            default:
+                Debug.Log("Botón seleccionado: " + button);
+                break;
+        }
     }
 
 }
