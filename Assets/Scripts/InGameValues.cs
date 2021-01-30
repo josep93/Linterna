19f90968b47cbada351abe8050b66f488d0e4e2a
josep93 <joseluisgomezmino@hotmail.com>
2021-01-30 19:05:28 +0100

    Juego - Implementado llaves y puertas

--- Assets/Scripts/InGameValues.cs
+++ Assets/Scripts/InGameValues.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class InGameValues : MonoBehaviour
 {
     public static bool paused;
+    public static bool haveKey;
 
     private void Awake()
     {
         paused = false;
+        haveKey = false;
     }
 }
