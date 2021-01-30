1fe242ca13165dce53d7b7e1854640adca336bfa
Jesús Miguel Pinto Sáez <jesuspinto@signlab.info>
2021-01-30 16:59:06 +0100

    working goal

---
+++ Assets/Scripts/GoalTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoalTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private int escenarioSiguiente;
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log(other);
+            SceneManager.LoadScene(escenarioSiguiente);
+        }
+    }
+}
--- Assets/Scripts/TriggerScript.cs
+++ Assets/Scripts/TriggerScript.cs
@@ -4,14 +4,9 @@ using UnityEngine;
 
 public class TriggerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+
+    private void OnTriggerEnter(Collider other)
     {
         
     }
