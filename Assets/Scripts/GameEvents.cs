9eaf1432f6f4648257b5a5dc0899a300ddeb6c55
Jesús Miguel Pinto Sáez <jesuspinto@signlab.info>
2021-01-30 15:26:43 +0100

    pause menu

---
+++ Assets/Scripts/GameEvents.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static InGameValues;
+
+public class GameEvents : MonoBehaviour
+{
+
+    public static GameEvents current;
+
+    private void Awake()
+    {
+        current = this;
+        PausePressed = Prueba;
+    }
+
+    public event Action<bool> PausePressed;
+    public void PressPause()
+    {
+        paused = !paused;
+        if (paused)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+        PausePressed(paused);
+    }
+
+    public void Prueba(bool prueba)
+    {
+        Debug.Log(prueba==true?"pausa":"continue");
+    }
+}
