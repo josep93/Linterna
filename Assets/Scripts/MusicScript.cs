2e78083fe4510528ea921b358564bab1c66043c5
josep93 <joseluisgomezmino@hotmail.com>
2021-01-30 20:05:59 +0100

    Juego - Implementado música ambientas

---
+++ Assets/Scripts/MusicScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MusicScript : MonoBehaviour
+{
+    
+    void Start()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    void OnSceneLoaded(Scene escena, LoadSceneMode mode)
+    {
+        if (escena.name.Equals("Menu"))
+        {
+            Destroy(this.gameObject);
+            SceneManager.sceneLoaded -= OnSceneLoaded;
+        }
+    }
+    
+}
