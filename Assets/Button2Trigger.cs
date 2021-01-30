847685dad4813ca38d8dfa720d52425df46fae63
Jesús Miguel Pinto Sáez <jesuspinto@signlab.info>
2021-01-31 00:28:03 +0100

    scene 3

---
+++ Assets/Button2Trigger.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static InGameValues;
+
+public class Button2Trigger : MonoBehaviour
+{
+    private bool loaded;
+    private Animator animator;
+    [SerializeField]
+    private Animator buttonAnimator;
+    [SerializeField]
+    private Collider collider;
+
+
+    private void Start()
+    {
+        loaded = false;
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag=="PlayerTrigger" && haveKey && !loaded)
+        {
+            loaded = true;
+            animator.SetTrigger("Load");
+        }
+
+        else if(other.gameObject.tag == "PlayerTrigger" && loaded)
+        {
+            animator.SetTrigger("Siege");
+        }
+    }
+
+    public void PressButton()
+    {
+        buttonAnimator.SetTrigger("ButtonPress");
+        collider.enabled = false;
+    }
+}
---
+++ Assets/RockTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private MeshRenderer roca;
+    private void OnDestroy()
+    {
+        Debug.Log("El juagor ha conseguido la llave: " + InGameValues.haveKey);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "PlayerTrigger")
+        {
+            roca.enabled = false;
+            InGameValues.haveKey = true;
+        }
+    }
+}
