6806207c6279b08e9b310991a2ba5f5afbf33a4a
josep93 <joseluisgomezmino@hotmail.com>
2021-01-30 18:09:58 +0100

    Menu - Implementado animación de título

---
+++ Assets/Scripts/Menu/AnimatorScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class AnimatorScript : MonoBehaviour
+{
+    public Animator animacion;
+    private Coroutine corrutina;
+    private int aux = 1;
+    private int temporizador = 4;
+
+    private void Start()
+    {
+        corrutina = StartCoroutine("lanzarAnimacion");
+        
+    }
+
+    private void OnDestroy()
+    {
+        StopCoroutine(corrutina);
+    }
+
+    IEnumerator lanzarAnimacion()
+    {
+        yield return new WaitForSeconds(temporizador);
+
+        switch (aux)
+        {
+            case 1:
+                temporizador = 15;
+                animacion.Play("LightTitle" + aux);
+                corrutina = StartCoroutine("lanzarAnimacion");
+
+                aux = 2;
+                break;
+
+            case 2:
+                temporizador = 15;
+                animacion.Play("LightTitle" + aux);
+                corrutina = StartCoroutine("lanzarAnimacion");
+
+                aux = 1;
+                break;
+        }
+
+        
+    }
+}
