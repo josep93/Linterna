12957bb19be484ee51776e57837f1af53d70c801
josep93 <joseluisgomezmino@hotmail.com>
2021-01-30 01:48:23 +0100

    Menu - Implementado seguimiento de luz

--- Assets/Scripts/Menu/MenuScript.cs
+++ Assets/Scripts/Menu/MenuScript.cs
@@ -3,7 +3,23 @@ using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
-    public GameObject miLuz;
+    public GameObject luzSelector;
+    public GameObject luzRaton;
+    public Camera camara;
+    private PlayerInputs playerInputs;
+
+    private void Awake()
+    {
+        playerInputs = new PlayerInputs();
+        playerInputs.Enable();
+    }
+
+    private void Update()
+    {
+        var vector = playerInputs.Player.MousePosition.ReadValue<Vector2>();
+        var vectorMundo = camara.ScreenToWorldPoint(new Vector3(vector.x, vector.y, 15.36f));
+        luzRaton.transform.position = new Vector3(vectorMundo.x, vectorMundo.y, -3);
+    }
 
     public void jugar()
     {
@@ -17,17 +33,16 @@ public class MenuScript : MonoBehaviour
 
     public void changeButtong(int button)
     {
-
         switch (button)
         {
             case 0:
                 //-6 5.75 -13
-                miLuz.transform.localPosition = new Vector3(miLuz.transform.localPosition.x, 5.75f, miLuz.transform.localPosition.z);
+                luzSelector.transform.position = new Vector3(luzSelector.transform.position.x, -0.5f, luzSelector.transform.position.z);
                 break;
 
             case 1:
                 //-6 2.75 -13
-                miLuz.transform.localPosition = new Vector3(miLuz.transform.localPosition.x, 2.75f, miLuz.transform.localPosition.z);
+                luzSelector.transform.position = new Vector3(luzSelector.transform.position.x, -3.2f, luzSelector.transform.position.z);
                 break;
 
             default:
