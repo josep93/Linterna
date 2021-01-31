fbddfeafdbfcd616fdcba030b3b88874dda173a4
josep93 <joseluisgomezmino@hotmail.com>
2021-01-31 02:15:28 +0100

    Juego - Ajustes menores level 2

--- Assets/Scripts/Triggers/ButtonTrigger.cs
+++ Assets/Scripts/Triggers/ButtonTrigger.cs
@@ -1,13 +1,18 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ButtonTrigger : MonoBehaviour
 {
+    [SerializeField]
+    private Light luz;
+    [SerializeField]
+    private GameObject esfera;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "PlayerTrigger")
         {
+            esfera.SetActive(false);
+            luz.enabled = false;
             GameEvents.current.DestroyDoorMethod();
             InGameValues.buttonDoorOpened = true;
         }
