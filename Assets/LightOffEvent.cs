c52b77f511f093bd73b6fb5e9f135b3198edd045
Jesús Miguel Pinto Sáez <jesuspinto@signlab.info>
2021-01-31 14:07:18 +0100

    scene 4 complete

---
+++ Assets/LightOffEvent.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightOffEvent : MonoBehaviour
+{
+    Light light;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = GetComponent<Light>();
+        GameEvents.current.StartCinematic += TurnOffLight;
+    }
+
+    public void TurnOffLight()
+    {
+        light.enabled = false;
+        StartCoroutine(TurnOnLightsIEnumerator());
+    }
+
+    public IEnumerator TurnOnLightsIEnumerator()
+    {
+        yield return new WaitForSeconds(1f);
+        light.enabled = true;
+        yield return new WaitForSeconds(0.05f);
+        light.enabled = false;
+        yield return new WaitForSeconds(0.2f);
+        light.enabled = true;
+        yield return new WaitForSeconds(0.05f);
+        light.enabled = false;
+        yield return new WaitForSeconds(0.4f);
+        light.enabled = true;
+    }
+}
