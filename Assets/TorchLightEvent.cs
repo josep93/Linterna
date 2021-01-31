b968cf16ab761412019aeca637b5cd9c4cbd647c
Jesús Miguel Pinto Sáez <jesuspinto@signlab.info>
2021-01-31 12:20:43 +0100

    scene 4 again

---
+++ Assets/TorchLightEvent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchLightEvent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameEvents.current.StartCinematic += TurnOffLights;
+    }
+
+    public void TurnOffLights()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
